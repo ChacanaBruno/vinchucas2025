492476aa3925edc7df29c3a3d0d46649688381de
ChacanaBruno <chacanabruno@gmail.com>
2025-06-16 16:53:42 -0300

    arreglo

--- TP-Integrador/src/ar/edu/unq/integrador/busqueda/CriterioBusqueda.java
+++ TP-Integrador/src/ar/edu/unq/integrador/busqueda/CriterioBusqueda.java
@@ -7,5 +7,26 @@ import ar.edu.unq.integrador.muestra.Muestra;
 
 public class CriterioBusqueda {
 
+	public List<FiltroDeBusqueda> getFiltros() {
+		return filtros;
+	}
 
+	public void setFiltros(List<FiltroDeBusqueda> filtros) {
+		this.filtros = filtros;
+	}
+
+	private List<FiltroDeBusqueda> filtros = new ArrayList<>();
+
+	public CriterioBusqueda(List<FiltroDeBusqueda> filtros) {
+		this.setFiltros(filtros);
+	}
+
+	public CriterioBusqueda agregarFiltro(FiltroDeBusqueda filtro) {
+		this.getFiltros().add(filtro);
+		return this;
+	}
+
+	public boolean cumple(Muestra muestra) {
+		return filtros.stream().allMatch(f -> f.cumple(muestra));
+	}
 }
